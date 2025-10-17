11f9a8a679c548513b5c7d7b9c51b652c7af6976
Josthin Paz <josthinpaz2@gmail.com>
2025-10-17 10:44:47 -0500

    Agregar carpeta docs para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo_list_editar/', // 👈 importante: debe tener slash inicial y final
 })
+
